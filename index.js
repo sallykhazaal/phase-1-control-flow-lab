1eb553cb7d6e55f8d87f263b443dbe85dda5c780
sallykhazaal <sallykhazaal@yahoo.com>
2023-01-31 13:56:04 -0500

    complete control lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let response;
+  if (someValue < 400) {
+    response = "This one is on me!";
+  } else if (someValue >= 400 && someValue < 2000) {
+    response = "That will be twenty bucks.";
+  } else if (someValue >= 2000 && someValue < 2500) {
+    response = "I will gladly take your thirty bucks.";
+  } else {
+    response = "No can do.";
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let response2;
+  response2 = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return response2;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTipGenerosity) {
+  let response3;
+  switch (someTipGenerosity) {
+    case "generous":
+      response3 = "Thank you so much.";
+      break;
+    case "not as generous":
+      response3 = "Thank you.";
+      break;
+    default:
+      response3 = "Bye.";
+  }
+  return response3;
 }
